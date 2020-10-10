8ac71f9129e1fd4dcaa41ce9e7fd70db8e82de0f
wanghongning <wanghongning@xin.com>
2020-10-10 21:02:30 +0800

    tree 未完成

--- tree.go
+++ tree.go
@@ -78,3 +78,71 @@ func (n *node) search(parts []string, depth int) *node {
 
 	return nil
 }
+
+// -----------------------
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type nodeType uint8
+
+const (
+	static nodeType = iota // default
+	root
+	urlparam
+	filepath
+)
+
+type newnode struct {
+	pattern  string
+	part     []rune
+	children []*newnode
+	isWild   bool
+	nType    nodeType
+	priority uint32
+	handle   HandlerFunc
+}
+
+func (n *newnode) addRoute(pattern string, handle HandlerFunc) {
+	n.priority++
+	if len(n.pattern) == 0 && len(n.children) == 0 {
+		n.nType = root
+		n.insert(pattern, handle)
+		return
+	}
+
+	var i int
+	l := min(len(pattern), len(n.pattern))
+	for j := 0; j < l; j++ {
+		if n.pattern[j] == pattern[j] {
+			i++
+			continue
+		}
+		break
+	}
+	if i == len(pattern) {
+		if n.handle != nil {
+			panic("a handle is already registered for path '" + pattern + "'")
+		}
+	} else if i < len(n.pattern) {
+		child := &newnode{
+			pattern:  n.pattern[i:],
+			isWild:   n.isWild,
+			nType:    static,
+			children: n.children,
+			priority: n.priority - 1,
+			handle: n.handle,
+		}
+		n.pattern = n.pattern[:i]
+		n.children = []*newnode{child}
+
+	}
+}
+
+func (n *newnode) insert(pattern string, handle HandlerFunc) {
+
+}
